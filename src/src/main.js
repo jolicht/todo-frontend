6273fea694eb54257dafbd211a58b000a031c7f2
jolicht <johannes@lichtenwallner.at>
2020-05-01 16:30:03 +0200

    display database time fetched from backend

--- src/src/main.js
+++ src/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAxios from "vue-axios";
+import Axios from "axios";
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, Axios);
 
 new Vue({
   render: h => h(App),
